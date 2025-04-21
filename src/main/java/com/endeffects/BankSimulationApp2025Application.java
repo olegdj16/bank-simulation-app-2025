ee2e0a84ed26c9a5d6ccbd4f120d0a373cacd4b4
olegdj16 <oleg16@gmail.com>
2025-04-20 22:54:18 -0500

    Initial commit

---
+++ src/main/java/com/endeffects/BankSimulationApp2025Application.java
@@ -0,0 +1,13 @@
+package com.endeffects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimulationApp2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankSimulationApp2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/endeffects/BankSimulationApp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endeffects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimulationApp2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
