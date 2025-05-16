7de16f5c5d4aaec1ff611fbf574d985a98e19423
olegdj16 <oleg16@gmail.com>
2025-05-16 14:44:27 -0500

    SKYN-11 - continuing bank simulation app

--- src/main/java/com/endeffects/BankSimulationApp2025Application.java
+++ src/main/java/com/endeffects/BankSimulationApp2025Application.java
@@ -19,14 +19,22 @@ public class BankSimulationApp2025Application {
     ApplicationContext container =
           SpringApplication.run(BankSimulationApp2025Application.class, args);
 
-    // get account and transaction service bean
-    AccountService accountService = container.getBean(AccountService.class);
-    TransactionService transactionService = container.getBean(TransactionService.class);
-
-    Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
-    Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.SAVINGS, 1L);
-
-    accountService.listAllAccount().forEach(System.out::println);
+//    // get account and transaction service bean
+//    AccountService accountService = container.getBean(AccountService.class);
+//    TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//    Account sender = accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
+//
+//    Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.CHECKING, 2L);
+////    Account receiver = null;
+//
+//    accountService.listAllAccount().forEach(System.out::println);
+//
+//    transactionService.makeTransfer(sender, receiver, BigDecimal.valueOf(400), new Date(), "Test transfer 1");
+//
+//    System.out.println(transactionService.findAllTransaction().get(0));
+//
+//    accountService.listAllAccount().forEach(System.out::println);
 
   }
 }
